b047ccff417356d73824f899ad93a6b42b32c742
Jonas Rembser <jonas.rembser@cern.ch>
2019-06-20 01:42:51 +0200

    cutflow can now invert and disable cuts

--- geeksw/data_formats/cutflow.py
+++ geeksw/data_formats/cutflow.py
@@ -1,24 +1,48 @@
 import numpy as np
 
 
+class CutFlag(object):
+    normal = 0
+    inverted = 1
+    disabled = 2
+    # in case a cut can't be inverted or disabled because the information to do so was lost,
+    # the cut will be fixed
+    fixed = 3
+
+    @staticmethod
+    def totext(flag):
+        if flag == CutFlag.normal:
+            return "normal"
+        if flag == CutFlag.inverted:
+            return "inverted"
+        if flag == CutFlag.disabled:
+            return "disabled"
+        if flag == CutFlag.fixed:
+            return "fixed"
+        raise ValueError("{0} is not a valid cut flag".format(flag))
+
+
+def apply_flag(mask, cut_flag):
+    if cut_flag == CutFlag.normal or cut_flag == CutFlag.fixed:
+        return mask
+    if cut_flag == CutFlag.disabled:
+        return np.ones(len(mask), dtype=np.bool)
+    if cut_flag == CutFlag.inverted:
+        return ~mask
+
+
 class Cutflow(object):
     @staticmethod
     def frommasks(masks, labels):
         cutflow = Cutflow()
-        _mask = np.ones_like(masks[0], dtype=np.bool)
+
         cutflow._labels = labels
-        cutflow._nbegin = len(masks[0])
-        efficiencies = []
-        _masks = []
-        for mask in masks:
-            _mask = np.logical_and(_mask, mask)
-            _masks.append(_mask)
-            efficiencies.append(float(np.sum(_mask)) / cutflow._nbegin)
+        cutflow._masks = masks
+        cutflow._flags = np.zeros(len(cutflow._masks), dtype=np.int) + CutFlag.normal
 
-        cutflow._efficiencies = np.array(efficiencies)
-        cutflow._nend = np.sum(_mask)
+        cutflow._n_events = [len(masks[0])]
 
-        cutflow._masks = _masks
+        cutflow._calculate_efficiencies()
 
         return cutflow
 
@@ -32,11 +56,11 @@ class Cutflow(object):
 
     @property
     def nbegin(self):
-        return self._nbegin
+        return self._n_events[0]
 
     @property
     def nend(self):
-        return self._nend
+        return self._n_events[-1]
 
     @property
     def efficiencies(self):
@@ -52,54 +76,113 @@ class Cutflow(object):
             cut_label = self._labels[-1]
 
         cut_index = self._labels.index(cut_label)
-        mask = self._masks[cut_index]
 
-        if len(array) == len(mask):
-            return array[mask]
+        mask_prev = np.ones(self.nbegin, dtype=np.bool)
 
-        for m in self._masks[:cut_index]:
-            if np.sum(m) == len(array):
-                return array[mask[m]]
+        for i in range(cut_index + 1):
+            if len(array) == self._n_events[i]:
+                masks = [apply_flag(m, self._flags[i + j]) for j, m in enumerate(self._masks[i : cut_index + 1])]
+                mask = np.logical_and.reduce(masks)
+                return array[mask[mask_prev]]
+            mask_prev = np.logical_and(mask_prev, self._masks[i])
+        if len(array) == self.nend:
+            return array
+
         raise ValueError("The cutflow can't automatically determine what to do.")
+
+    def _calculate_efficiencies(self):
+
+        efficiencies = []
+        n_events = [self.nbegin]
+
+        total_mask = np.ones(self.nbegin, dtype=np.bool)
+
+        for mask, flag in zip(self._masks, self._flags):
+            total_mask = np.logical_and(total_mask, apply_flag(mask, flag))
+            n_remaining = np.sum(total_mask)
+            efficiencies.append(float(n_remaining) / self.nbegin)
+            n_events.append(n_remaining)
+
+        self._efficiencies = np.array(efficiencies)
+        self._n_events = np.array(n_events)
+
+    def invert(self, cut_label):
+        cut_index = self._labels.index(cut_label)
+        if self._flags[cut_index] == CutFlag.fixed:
+            raise ValueError("cut " + cut_label + " can't be toggled, the information to do so was lost.")
+        if self._flags[cut_index] == CutFlag.disabled:
+            raise ValueError("cut " + cut_label + " can't be toggled because it's disabled.")
+
+        if self._flags[cut_index] == CutFlag.normal:
+            self._flags[cut_index] = CutFlag.inverted
+            self._calculate_efficiencies()
+        elif self._flags[cut_index] == CutFlag.inverted:
+            self._flags[cut_index] = CutFlag.normal
+            self._calculate_efficiencies()
+
+    def disable(self, cut_label):
+        cut_index = self._labels.index(cut_label)
+        if self._flags[cut_index] == CutFlag.fixed:
+            raise ValueError("cut " + cut_label + " can't be disabled, the information to do so was lost.")
+
+        if self._flags[cut_index] == CutFlag.inverted:
+            self.invert(cut_label)
+
+        self._flags[cut_index] = CutFlag.disabled
+        self._calculate_efficiencies()
+
+    def enable(self, cut_label):
+        cut_index = self._labels.index(cut_label)
+        if self._flags[cut_index] == CutFlag.fixed or self._flags[cut_index] == CutFlag.normal:
+            return
+
+        self._flags[cut_index] = CutFlag.normal
+        self._calculate_efficiencies()
 
     def __repr__(self):
         s = "<Cutflow"
-        for label, eff in zip(self._labels, self._efficiencies):
+        for label, eff, flag in zip(self._labels, self._efficiencies, self._flags):
             if s[-1] != "w":
                 s += ","
             if eff * 100 > 0.01:
-                s += " (" + label + ": {0:.4f} %)".format(eff * 100)
+                s += " (" + label + ": {0:.4f} %)".format(eff * 100) + " (" + CutFlag.totext(flag) + ")"
             else:
-                s += " (" + label + ": {0:.2e})".format(eff)
+                s += " (" + label + ": {0:.2e})".format(eff) + " (" + CutFlag.totext(flag) + ")"
         s += ">"
         return s
 
     def __str__(self):
         s = "Cutflow:"
         i = 0
-        for label, eff in zip(self._labels, self._efficiencies):
+        for label, eff, flag in zip(self._labels, self._efficiencies, self._flags):
             i = i + 1
             if eff * 100 > 0.01:
-                s += "\n    " + str(i) + ". " + label + ": {0:.4f} %)".format(eff * 100)
+                eff_str = "{0:.4f} %)".format(eff * 100)
             else:
-                s += "\n    " + str(i) + ". " + label + ": {0:.2e})".format(eff)
+                eff_str = "{0:.2e})".format(eff)
+            s += "\n    " + str(i) + ". " + label + ": " + eff_str + " (" + CutFlag.totext(flag) + ")"
         return s
 
     def __mul__(self, other):
-        if self._nend != other._nbegin:
+        if self.nend != other.nbegin:
             raise ValueError("cutflows don't seem to match from the number of events")
 
         cutflow = Cutflow()
 
         cutflow._labels = self._labels + other._labels
         cutflow._efficiencies = np.concatenate([self._efficiencies, other._efficiencies * self._efficiencies[-1]])
-        cutflow._nbegin = self._nbegin
-        cutflow._nend = other._nend
+        cutflow._n_events = np.concatenate([self._n_events, other._n_events[1:]])
 
         cutflow._masks = [m.copy() for m in self._masks]
+
+        total_mask = np.logical_and.reduce(self._masks)
+
         for m in other._masks:
-            cutflow._masks.append(self._masks[-1].copy())
-            cutflow._masks[-1][cutflow._masks[-1]] = m
+            cutflow._masks.append(total_mask.copy())
+            cutflow._masks[-1][total_mask] = m
+
+        cutflow._flags = np.zeros(len(cutflow._masks), dtype=np.int) + CutFlag.normal
+        cutflow._flags[: len(self._masks)] = CutFlag.fixed
 
         return cutflow
 
@@ -145,9 +228,8 @@ class Cutflow(object):
 
         cutflow = Cutflow()
         cutflow._labels = cutflows[0]._labels
-        cutflow._nbegin = np.sum([cf._nbegin for cf in cutflows])
-        cutflow._nend = np.sum([cf._nend for cf in cutflows])
+        cutflow._n_events = np.sum([cf._n_events for cf in cutflows], axis=0)
 
-        cutflow._efficiencies = np.sum([cf._efficiencies * cf._nbegin for cf in cutflows], axis=0) / cutflow._nbegin
+        cutflow._efficiencies = np.sum([cf._efficiencies * cf.nbegin for cf in cutflows], axis=0) / cutflow.nbegin
 
         return cutflow
