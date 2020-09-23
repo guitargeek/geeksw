d6e774824eae713e68cf23ae242933be9df52fcf
Jonas Rembser <jonas.rembser@cern.ch>
2020-09-23 18:39:35 +0200

    improve the EFT weights handling

--- geeksw/analysis/eft.py
+++ geeksw/analysis/eft.py
@@ -21,18 +21,16 @@ wwzz_dim8_operators = [
 ]
 
 
-def get_df_mg_reweighting_info(events):
+def get_df_mg_reweighting_info(mg_reweighting_title):
     import io
 
-    csv_str = events["LHEWeight_mg_reweighting"].title.decode("utf-8")
-    return pd.read_csv(io.StringIO(csv_str), lineterminator=";", skiprows=1)
+    return pd.read_csv(io.StringIO(mg_reweighting_title), lineterminator=";", skiprows=1)
 
 
-def get_df_mg_reweighting(events, standard_model=None):
-    gen_weight = events.array("genWeight")
+def get_df_mg_reweighting(gen_weight, mg_reweighting, mg_reweighting_title, standard_model=None):
 
-    df_info = get_df_mg_reweighting_info(events)
-    mg_reweighting = gen_weight * events.array("LHEWeight_mg_reweighting")
+    df_info = get_df_mg_reweighting_info(mg_reweighting_title)
+    mg_reweighting = gen_weight * mg_reweighting
     df = pd.DataFrame(mg_reweighting.flatten().reshape((len(mg_reweighting), len(mg_reweighting[0]))))
     df.columns = df_info["id"].values
 
@@ -45,21 +43,21 @@ def get_df_mg_reweighting(events, standard_model=None):
 
 
 class EFTReweighter(object):
-    def __init__(self, events, xsec=1.0, lumi=1.0):
+    def __init__(self, gen_weight, mg_reweighting, mg_reweighting_title, xsec=1.0, lumi=1.0):
         def get_coefficients(op):
             v = np.array([-1.0, 0, 1])
             w = df_weights[[f"EFT__{op}_m1", "EFT__SM", f"EFT__{op}_1"]].values
             x = np.tile(np.vstack([v ** 2, v, np.ones_like(v)]).T, (len(w), 1, 1))
             return np.linalg.solve(x, w)
 
-        df_weights = get_df_mg_reweighting(events, standard_model="EFT__SM") * xsec * lumi
+        df_weights = get_df_mg_reweighting(gen_weight, mg_reweighting, mg_reweighting_title, standard_model="EFT__SM") * xsec * lumi
 
         self.coef_ = {}
 
         for op in wwzz_dim8_operators:
             self.coef_[op] = get_coefficients(op)
 
-    def __call__(self, operator, value):
+    def __call__(self, operator, value, subtract_sm=False):
 
         if hasattr(value, "__len__"):
             dtype = object
@@ -67,8 +65,15 @@ class EFTReweighter(object):
             if value == 0.0:
                 # if the value is zero, we deal with SM and can just take the offsets of any operator,
                 # which should be the same
+                if subtract_sm:
+                    return np.zeros_like(self.coef_["FT0"][:, 2])
                 return self.coef_["FT0"][:, 2]
             dtype = type(value)
 
         x = np.array([value ** 2, value, 1.0], dtype=dtype)
-        return np.matmul(self.coef_[operator], x)
+        weights = np.matmul(self.coef_[operator], x)
+
+        if subtract_sm:
+            return weights - self.coef_["FT0"][:, 2]
+
+        return weights
